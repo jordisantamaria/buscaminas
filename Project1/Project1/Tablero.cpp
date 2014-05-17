b48ed033d12e774683659a290dd598db727de43e
jordiviva <santjordiporto@hotmail.com>
2014-05-17 12:33:41 +0200

    Genera les minas i destapa les caselles colocant el numero de les mines properes que té
    Falta arreglar la recursivitat, quan no hi han mines properes no destapa les adjacents.

--- Project1/Project1/Tablero.cpp
+++ Project1/Project1/Tablero.cpp
@@ -28,45 +28,147 @@ CTablero::CTablero(int width, int height, int minas)
 			casella[i][j].setnumMinasProperas(0);
 		}
 	}
+	inicialitzarMinas();
+}
+
+void CTablero::inicialitzarMinas()
+{
+	int randomX, randomY;
+	int numMinas = 0;
+
+	while(numMinas<minas)
+	{
+		randomX = numAleatori(width-1);
+		randomY = numAleatori(height-1);
+		if(casella[randomX][randomY].getNumMinasProperas() == 0)
+		{
+		casella[randomX][randomY].setnumMinasProperas(9);
+		numMinas++;
+		}
+	}
 }
 
 void CTablero::draw()
 {
-	char c = 178;
+	char c = CASELLA_SENSE_DESTAPAR;
 	int i = 0;
 	int j = 0;
 
-	while(i<height)
-{
-	if(casella[i][j].getDestapat() == false)
+	while(j<height)
 	{
-	cout << c;//casella[i][j].getNumMinasProperas();
-	}else
-	{
-		cout <<casella[i][j].getNumMinasProperas();
-	}
-	j++;
-	if(j==width-1)
-	{
-		j=0;
+		//Si encara no s'ha destapat, dibuixa el caracter de CASELLA_SENSE_DESTAPAR
+		if(casella[i][j].getDestapat() == false)
+		{
+			cout << c;
+		//si está destapada, dibuixa el numero de minas adjacents.
+		}else
+		{
+			cout <<casella[i][j].getNumMinasProperas();
+		}
 		i++;
-		cout <<endl;
+		//si ha arribat a la ultima columna, fes un salt de linia i continua per la seguent fila.
+		if(i==width-1)
+		{
+			i=0;
+			j++;
+			cout <<endl;
+		}
 	}
-}
 }
 
-void CTablero::moviment()
+void CTablero::moviment(int &state)
 {
 	int x, y;
 	demanarCoordenadaX(x);
 	demanarCoordenadaY(y);
-	destaparCasella(x,y);
+	state = destaparCasella(x,y);
+}
+void CTablero::setNumMinasProperas(int x, int y)
+{
+
+	int numMinasAdjacents = 0;
+	int i = x-1;
+	int j = y-1;
+	while(i<=x+1)
+	{
+		
+			if(casella[i][j].getNumMinasProperas() == 9)
+			{
+				numMinasAdjacents++;
+			}
+			j++;
+		if(j==y+1)
+		{
+			i++;
+			j=y-1;
+		}
+		
+	}
+	casella[x][y].setnumMinasProperas(numMinasAdjacents);
+	cout << "El numero de minas properas es" << numMinasAdjacents<<endl;
 }
 
-void CTablero::destaparCasella(int x, int y)
+int CTablero::min(int num, int min)
 {
-	casella[x-1][y-1].setDestapat(true);
+	if(num < min)
+	{
+	return min;
+	}else{
+		return num;
+	}
+}
+int CTablero::max(int num, int max)
+{
+	if(num > max)
+	{
+	return max;
+	}else{
+		return num;
+	}
+}
+
+
+int CTablero::destaparCasella(int x, int y)
+{
+	int posX = x-1;
+	int posY = y-1;
+
+	// SI HI HA MINA EN AQUELLA POSICIO, MOSTRA EL TAULER DE NOU I ACABA LA PARTIDA
+	if(casella[posX][posY].getNumMinasProperas() == 9)
+	{
+		casella[posX][posY].setDestapat(true);
+		draw();
+		cout << "Aquesta casella era una bomba, GAME OVER" <<endl;
+		return SALIR;
+	}
+
+	// SINO, DESTAPA AQUELLA CASELLA I LI ASIGNA EL NUMERO CORRESPONENT, SI AQUELLA CASELLA NO TE MINAS PROPERAS, CRIDA RECURSIVITAT A LES ADJACENTS.
+	else
+	{
+	setNumMinasProperas(posX, posY);
+
+	casella[posX][posY].setDestapat(true);
 	cout << "La casella" <<x << y << "destapat = " << casella[x][y].getDestapat() <<endl;
+
+	// RECURSIVITAT PER A LES CASELLES VEINES
+
+	if(casella[posX][posY].getNumMinasProperas() ==0)
+	{
+		for(int i = min(posX-1, 0); i <= max(posX+1, width-1); i++)
+		{
+			for(int j = min(posY-1, 0); j <= max(posY+1, height-1); j++)
+			{
+				if(casella[posX][posY].getDestapat() == false)
+				{
+					destaparCasella(i, j);
+				}
+			}
+		}
+	}
+
+
+	}
+	return GAMEPLAY;
 }
 void CTablero::demanarCoordenadaX(int &x)
 {
@@ -87,6 +189,13 @@ void CTablero::demanarCoordenadaY(int &y)
 		cout << "Coordenada Y Incorrecte" <<endl;
 		demanarCoordenadaY(y);
 	}
+}
+
+int CTablero::numAleatori (int maxNum)
+{
+	
+//	std::srand(std::time(0));//PARA QUE LA SEGUENCIA DE NUMEROS ALEATORIOS NO SEA SIEMPRE LA MISMA
+	return (rand() % maxNum);
 }
 CTablero::~CTablero(void)
 {
