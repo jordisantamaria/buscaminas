4c219ccad5e9b2006c3669b65183db2ef60d611c
jordiviva <santjordiporto@hotmail.com>
2014-05-17 12:34:32 +0200

    Nous metodes privats i ara moviment retorna SALIR si li ha explotat la bomba i s'acaba el joc

--- Project1/Project1/Tablero.h
+++ Project1/Project1/Tablero.h
@@ -2,6 +2,14 @@
 
 #include "Casella.h"
 
+enum estatsGame{
+	MENU,
+	GAMEPLAY,
+	SALIR
+};
+
+const int MINA = 9;
+const int CASELLA_SENSE_DESTAPAR = 178;
 class CTablero
 {
 public:
@@ -10,13 +18,21 @@ public:
 	~CTablero(void);
 
 	void draw();
-	void moviment();
-	void destaparCasella(int x, int y);
+	void moviment(int &state);
+	
 
 private:
 
+	int numAleatori (int maxNum);
 	void demanarCoordenadaX(int &x);
 	void demanarCoordenadaY(int &y);
+	int destaparCasella(int x, int y);
+	void setNumMinasProperas(int x, int y);
+	void inicialitzarMinas();
+	int min(int num, int min);
+	int max(int num, int max);
+	
+
 	int width;
 	int height;
 	int minas;
